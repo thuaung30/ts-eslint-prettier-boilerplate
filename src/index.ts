a6f92da1b4fae6c29469cee662904d8ddf0a4a55
thuaung30 <thuhtetaung.d2@gmail.com>
2021-11-18 02:57:37 +0630

    add swagger and jest

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-const foo = 'foo';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import router from './routes';
+import swaggerUi from 'swagger-ui-express';
 
-console.log(foo);
+const PORT = parseInt(process.env.PORT as string) || 3000;
+
+const app: Application = express();
+
+// middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(express.static('public'));
+
+// swagger documentation
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
+  })
+);
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
